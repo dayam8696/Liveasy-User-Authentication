1634330bbd9da3c87d7718e91f30c7c4f2f7e97e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-12 01:14:23 +0530

    added firebase

--- app/src/main/java/com/example/liveasy/fragments/ProfileSelectFragment.kt
+++ app/src/main/java/com/example/liveasy/fragments/ProfileSelectFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
+import com.example.liveasy.R
 import com.example.liveasy.databinding.ProfileSelectFragmentBinding
 
-class ProfileSelectFragment:Fragment() {
+class ProfileSelectFragment : BaseFragment() {
     private val binding by lazy { ProfileSelectFragmentBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,15 +17,8 @@ class ProfileSelectFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return binding.root
-    }
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.selectpro.setOnCheckedChangeListener { radiogroup, checkedId ->  }
     }
-    
-    
 
 
 }

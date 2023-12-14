610010704fbd76c3ccc0c3b7fb532caa25058a7c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-14 18:43:43 +0530

    implemented firebase

--- app/src/main/java/com/example/liveasy/fragments/MobileFragment.kt
+++ app/src/main/java/com/example/liveasy/fragments/MobileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.liveasy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.liveasy.R
 import com.example.liveasy.databinding.FragmentMobileBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.i18n.phonenumbers.PhoneNumberUtil
+import com.google.i18n.phonenumbers.Phonenumber
+import java.util.concurrent.TimeUnit
 
-class MobileFragment: Fragment(){
+class MobileFragment : BaseFragment() {
     private val binding by lazy { FragmentMobileBinding.inflate(layoutInflater) }
+    private val mAuth = FirebaseAuth.getInstance()
+
+    private var isResend = false
+
+    companion object {
+        lateinit var verificationCode: String
+        lateinit var resendingToken: PhoneAuthProvider.ForceResendingToken
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,9 +39,87 @@ class MobileFragment: Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-   binding.contbtn.setOnClickListener {
-       findNavController().navigate(R.id.action_mobileFragment_to_verifyFragment3)
-   }
+        setupClicklisteneres()
+//        binding.contbtn.setOnClickListener {
+//            findNavController().navigate(R.id.action_mobileFragment_to_verifyFragment3)
+//        }
+    }
+
+    private fun setupClicklisteneres() {
+        binding.contbtn.setOnClickListener {
+            sendOtpAndNavigate()
         }
     }
+
+    private fun sendOtpAndNavigate() {
+        if (isPhoneNumberValid(
+                binding.editTextPhoneNumber.text.toString().trim(),
+                binding.countryCode.selectedCountryCode
+            )
+        ) {
+            sendOtp(
+                binding.countryCode.selectedCountryCode,
+                binding.editTextPhoneNumber.text.toString().trim()
+            )
+//            findNavController().navigate(R.id.action_mobileFragment_to_verifyFragment3)
+        } else showToast("not ok")
+    }
+
+    private fun isPhoneNumberValid(phoneNumber: String, countryCode: String): Boolean {
+        val phoneNumberUtil = PhoneNumberUtil.getInstance()
+        val phoneNumberProto = Phonenumber.PhoneNumber()
+        return try {
+            phoneNumberProto.countryCode = countryCode.toInt()
+            phoneNumberProto.nationalNumber = phoneNumber.toLong()
+            phoneNumberUtil.isValidNumber(phoneNumberProto)
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
+    private fun sendOtp(countryCode: String, phoneNumber: String) {
+        val builder = PhoneAuthOptions.newBuilder(mAuth)
+            .setPhoneNumber("+$countryCode$phoneNumber")
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+//                    verifyOtp(credential)
+//                    binding.progressBar.visibility = View.GONE
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    showToast("Too many requests \nTry again after some time")
+//                    binding.progressBar.visibility = View.GONE
+                }
+
+                override fun onCodeSent(
+                    p0: String,
+                    forceResendingToken: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    super.onCodeSent(p0, forceResendingToken)
+                    verificationCode = p0
+                    resendingToken = forceResendingToken
+//                    isResend = true
+//                    binding.verifyOtp.visibility = View.VISIBLE
+                    showToast("OTP Sent Successfully")
+                    findNavController().navigate(R.id.action_mobileFragment_to_verifyFragment3)
+//                    binding.progressBar.visibility = View.GONE
+//                    startResendTimer()
+                }
+            })
+
+        if (isResend) {
+            PhoneAuthProvider.verifyPhoneNumber(
+                builder.setForceResendingToken(resendingToken).build()
+            )
+        } else {
+            Log.d("phoneno", "+$countryCode$phoneNumber")
+            PhoneAuthProvider.verifyPhoneNumber(
+                builder.build()
+            )
+        }
+    }
+}
+
 
--- app/src/main/java/com/example/liveasy/fragments/VerifyFragment.kt
+++ app/src/main/java/com/example/liveasy/fragments/VerifyFragment.kt
@@ -1,6 +1,7 @@
 package com.example.liveasy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.liveasy.R
 import com.example.liveasy.databinding.FragmentVerifyPageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 
-class VerifyFragment :Fragment() {
+class VerifyFragment :BaseFragment() {
     private val binding by lazy { FragmentVerifyPageBinding.inflate(layoutInflater) }
+
+    private val mAuth = FirebaseAuth.getInstance()
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,8 +29,43 @@ class VerifyFragment :Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.verifycontbtn.setOnClickListener {
-            findNavController().navigate(R.id.action_verifyFragment3_to_profileSelectFragment)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+//        binding.verifycontbtn.setOnClickListener {
+//            val credential = PhoneAuthProvider.getCredential(
+//                MobileFragment.verificationCode,
+//                binding.otpView.
+//            )
+//
+//        }
+
+        binding.otpView.setOtpCompletionListener {otp->
+            Log.d("Actual Value", otp)
+            val credential = PhoneAuthProvider.getCredential(
+                MobileFragment.verificationCode,
+                otp
+            )
+
+            verifyOtp(credential)
+        }
+    }
+
+    private fun verifyOtp(credential: PhoneAuthCredential) {
+//        binding.progressBar.visibility = View.VISIBLE
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+//                isOtpVerified = true
+//                binding.verifyOtp.visibility = View.GONE
+                showToast("OTP Verified Successfully")
+//                binding.progressBar.visibility = View.GONE
+//                binding.resendTimer.visibility = View.GONE
+            } else {
+                showToast("OTP Verification Failed")
+//                binding.progressBar.visibility = View.GONE
+            }
         }
     }
 

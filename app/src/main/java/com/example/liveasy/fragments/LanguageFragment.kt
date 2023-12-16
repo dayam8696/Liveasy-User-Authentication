62e199c5494962a59378b0dc6fe9183d6e9c5595
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-16 20:10:44 +0530

    final touch

--- app/src/main/java/com/example/liveasy/fragments/LanguageFragment.kt
+++ app/src/main/java/com/example/liveasy/fragments/LanguageFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -29,33 +30,11 @@ class LanguageFragment : BaseFragment() {
             findNavController().navigate(R.id.action_languageFragment_to_mobileFragment)
         }
 
-        setupSpinner()
+
 
 
     }
 
-    private fun setupSpinner() {
-        val spinnerList = mutableListOf("English", "Hindi", "Gujarati")
-        val adapter = ArrayAdapter(
-            requireContext(),
-            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-            spinnerList
-        )
-        binding.spinner.adapter = adapter
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                showToast(spinnerList[position])
-            }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
 
-            }
-
-        }
-    }
 }
